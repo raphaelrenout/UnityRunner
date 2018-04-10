35ec6da2b86cde0af3583029160aeaf79032d413
BenjaminLemaitre <32834667+BenjaminLemaitre@users.noreply.github.com>
2018-04-10 16:50:11 +0200

    player controller

---
+++ UnityRunnser/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody myRigidBody;
+    public float speed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.D))
+            myRigidBody.AddForce(speed * Vector3.right);
+
+        if (Input.GetKey(KeyCode.Q))
+            myRigidBody.AddForce(speed * Vector3.left);
+
+        if (Input.GetKey(KeyCode.Z))
+            myRigidBody.AddForce(speed * Vector3.forward);
+
+        if (Input.GetKey(KeyCode.S))
+            myRigidBody.AddForce(speed * Vector3.back);
+
+        if (Input.GetKey(KeyCode.Space))
+            myRigidBody.AddForce(speed * Vector2.up);
+	}
+}
