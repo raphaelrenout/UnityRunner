d9ab5fa2ce29eb508047da6d8ebd44503c4f5790
BenjaminLemaitre <32834667+BenjaminLemaitre@users.noreply.github.com>
2018-04-11 14:34:06 +0200

    Players movements

--- UnityRunnser/Assets/Scripts/PlayerController.cs
+++ UnityRunnser/Assets/Scripts/PlayerController.cs
@@ -5,28 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody myRigidBody;
+    private Transform myTransform;
+    private Collider col;
+
     public float speed = 0.05f;
 
 	// Use this for initialization
 	void Start () {
         myRigidBody = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+        myTransform = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey(KeyCode.D))
-            myRigidBody.AddForce(speed * Vector3.right);
+            myRigidBody.velocity = transform.right * speed;
 
         if (Input.GetKey(KeyCode.Q))
-            myRigidBody.AddForce(speed * Vector3.left);
+            myRigidBody.velocity = -transform.right * speed;
 
         if (Input.GetKey(KeyCode.Z))
-            myRigidBody.AddForce(speed * Vector3.forward);
+            myRigidBody.velocity = transform.forward * speed;
 
         if (Input.GetKey(KeyCode.S))
-            myRigidBody.AddForce(speed * Vector3.back);
+            myRigidBody.velocity = -transform.forward * speed;
 
         if (Input.GetKey(KeyCode.Space))
-            myRigidBody.AddForce(speed * Vector2.up);
+            myRigidBody.velocity = transform.up * speed;
+
+        OnTriggerEnter(col);
 	}
+
+    void OnTriggerEnter(Collider col) {
+        if (col.name == "WaterBasicDaytime")
+        {
+            Physics.gravity /= 100;
+            speed = 300;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Water")
+        {
+            Physics.gravity *= 10;
+        }
+    }
 }
